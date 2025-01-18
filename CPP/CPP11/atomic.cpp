2c7516c2790495c850fcdee54bdf5db0e5c5c156
hexne <printfne@gmail.com>
2025-01-18 19:11:58 +0800

    补充atomic类型的使用

--- CPP/CPP11/atomic.cpp
+++ CPP/CPP11/atomic.cpp
@@ -15,6 +15,18 @@ int main () {
     std::atomic<int> flag_atomic{};
     flag_atomic += 1;
 
+    // 检查原子对象是否免锁
+    flag_atomic.is_lock_free();
+
+    // 使用参数1 原子地替换当前值，并按照参数2指定内存序, 默认为 memory_order_seq_cst
+    flag_atomic.store(10,std::memory_order_seq_cst);
+    
+    // 原子地加载并返回当前原子变量的当前值，按照参数指定内存序，默认为 memory_order_seq_cst 
+    flag_atomic.load(std::memory_order_seq_cst);
+
+
+
+
 
 
 
